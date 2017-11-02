e53a2cd152f56d17f27bb3f28335d52550e37aa3
SalcedoJose <josemasal@gmail.com>
2017-11-01 17:49:39 -0700

    Added capsule as player model. Added scripts to respond to mouse movement (that is not working).

---
+++ Assets/main/scripts/MouseLook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+	public enum RotationAxes {
+		MouseXAndY = 0,
+		MouseX = 1,
+		MouseY = 2
+	}
+
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+
+	public float sensitivityHor = 9.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (axes == RotationAxes.MouseX) {
+			// Horizontal rotation here.
+			transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor
+					, 0);
+		} else if (axes == RotationAxes.MouseY) {
+			// Vertical rotation here.
+		} else {
+			// Both horizontal and vertical rotation.
+		}
+		
+	}
+}
